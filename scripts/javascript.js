e13b21a3bc60128faefa30a242683d837c557ec1
nimishgo <nimish.2711@gmail.com>
2022-06-28 22:26:54 +0530

    added js scripts

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,24 +1,55 @@
 const pad = document.querySelector('.pad'); 
 const button = document.querySelector('button');
+const colorPick = document.querySelector('.colorPicker');
+const slider = document.querySelector('.slider');
+const reset = document.querySelector('.reset');
+let RGB = document.querySelector('.checkbox');
 
-let size = 0;
-button.addEventListener('click', (e) => {
-while(size < 256)
-{
-const box = document.createElement('div');
-box.className = 'miniBox';
-pad.appendChild(box);
-box.style.cssText = 'display:flex-inline ; flex-wrap : wrap ; border : 1px solid black; width : 30px ; height : 30px';
-console.log(box);
-size++;
-}
+let valpix;
+let checkRGB;
+let cellColor;
+
+RGB.addEventListener('change', (e) => {
+    if(RGB.checked == true)
+    {
+        checkRGB = true;
+    }
+    else{
+        checkRGB = false;
+    }
+    // cellColor = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)} , ${Math.floor(Math.random()*255)})`;
 });
 
-//         for (let index = 0; index < 256; index++) {
-//             box[index] = document.createElement('div');
-//             pad.appendChild(box);
-//             box.className = 'miniBox';
-//         }
-//     });
+colorPick.addEventListener('input' , (e) => {
+    cellColor = e.target.value;
+    // console.log(e.target.value);
+});
 
-    
+slider.addEventListener('change', (e) => {
+
+   valpix = e.target.value;
+   console.log(valpix);
+   let size = 0;
+   
+   while(size < valpix*valpix)
+   {
+    const box = document.createElement('div');
+    box.className = 'miniBox';
+    pad.appendChild(box);
+        box.style.cssText = `display:flex-inline ; flex-wrap : wrap ; border : 0px solid black; width : ${Math.floor(512/valpix)}px ; height : ${Math.floor(512/valpix)}px`;
+        box.addEventListener('pointerenter', e => {
+        
+        if(checkRGB){
+            cellColor = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)} , ${Math.floor(Math.random()*255)})`;
+        }
+
+        e.target.style.backgroundColor = `${cellColor}`;
+    });
+    size++;
+   }
+});
+
+
+reset.addEventListener('click', () => {
+    location.reload();
+});
